1f7b077c17fa8edb58f92cff2099fa86b02c5e4b
Harshan.R.Arasa <harshanarasa@gmail.com>
2025-06-13 09:06:35 +0530

    cloning my repo

---
+++ p2b.java
@@ -0,0 +1,9 @@
+public class p3{
+    publc static void main(String []args)
+    {
+    System.out.println("it is used for stash");
+    System.out.println("it is used for cloning my repo");
+    return 0;
+
+    }
+}
